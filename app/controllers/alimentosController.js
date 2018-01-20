ca9268c0347e320525b82bb502101d46a65c5705
Diana Coello <DianaCoello@users.noreply.github.com>
2018-01-20 12:41:32 -0500

    insertarAlimento

--- app/controllers/alimentosController.js
+++ app/controllers/alimentosController.js
@@ -5,11 +5,41 @@ angular.module('InsuControl')
 	 function($scope, $rootScope, $location, $http, 
 		localStorageService) {
 
-	 	var url = "http://localhost/pdo_servicios/Ws_Ic/vista/obtenerAlimento.php";
-		$http.post(url)
+	 	var obtenerAlimento = "http://localhost/pdo_servicios/Ws_Ic/vista/obtenerAlimento.php";
+		var obtenerCategoria = "http://localhost/pdo_servicios/Ws_Ic/vista/obtenerCategoria.php";
+		var guardarAlimento = "http://localhost/pdo_servicios/Ws_Ic/vista/guardarAlimento.php";
+		var buscarAlimento = "http://localhost/pdo_servicios/Ws_Ic/vista/guardarAlimento.php";
+
+		$http.post(obtenerAlimento)
 		  .then(function(response) {
 		      $scope.data = response.data.alimentos;
-		      console.log($scope.data);
 		  });
-	}
+
+		$http.post(obtenerCategoria)
+		  .then(function(response) {
+		      $scope.categoria = response.data.categoria;
+		 	});
+
+		
+		$scope.insertarAlimentos = function(){
+			$http.post(guardarAlimento, {'id_categoria': $scope.id_categoria, 
+				'nombre': $scope.nombre, 'porcion': $scope.porcion,
+				'peso_porcion': $scope.peso_porcion, 'gramos_ch': $scope.gramos_ch, 
+				'cant_porcion': $scope.cant_porcion})
+			.then(function(response){
+				console.log($scope.id_categoria);
+		      console.log(response);
+			});
+		}
+
+		$scope.buscarAlimentos = function(){
+			$http.post(buscarAlimento, {'id_ch': $scope.id_categoria})
+			.then(function(response){
+				console.log($scope.id_ch);
+		      console.log(response);
+			});
+		}
+
+
+	}//fin function principal
 ]);
