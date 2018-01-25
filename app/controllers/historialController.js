d083701a54c65704d99da398a673d10c5d5285a7
EdwinRobalino <edboroba@gmail.com>
2018-01-25 00:33:29 -0500

    Rango historial

--- app/controllers/historialController.js
+++ app/controllers/historialController.js
@@ -1,12 +1,10 @@
 'use strict';
  
-angular.module('InsuControl').config(['ChartJsProvider', function (ChartJsProvider) {
-    // Configure all charts
-    
-  }])
+angular.module('InsuControl')
 .controller('historialController', ['$scope', '$rootScope', '$location', '$http', 'localStorageService', 
 	 function($scope, $rootScope, $location, $http, 
 		localStorageService) {
+         Chart.defaults.global.defaultFontColor = '#fff';
          
          
         //var obtenerHistorial = "http://insucontrol.life/pdo_servicios/Ws_Ic/vista/obtenerHistorial.php"; 
@@ -23,17 +21,29 @@ angular.module('InsuControl').config(['ChartJsProvider', function (ChartJsProvid
          $scope.sGlucosa = ['Glucosa'];
          $scope.sCarbohidratos = ['Carbohidratos'];
          $scope.sInsulina = ['Insulina'];
+         
+         
+        var fHoy = new Date();
+        var fSemana = new Date();
+        fSemana.setDate(fSemana.getDate() - 7);
+
+        $scope.sfHoy = (fHoy.getFullYear() + "-"+(fHoy.getMonth()+1)+"-"+fHoy.getDate()+" 23:59:00");
+         $scope.sfPasado = (fSemana.getFullYear() + "-"+(fSemana.getMonth()+1)+"-"+fSemana.getDate()+" 00:00:00");
+         
+        console.log ("hoy= " + $scope.sfHoy);
+         console.log ("pasado= "+$scope.sfPasado); 
 
 		$scope.mostrarHistorial = function(){
-			$http.post(obtenerHistorial, {'id_usuario': $rootScope.paciente_id})
+			$http.post(obtenerHistorial, {'id_usuario': $rootScope.paciente_id, 'fecha1': $scope.sfPasado, 'fecha2':$scope.sfHoy})
 		  	.then(function(response) {
-                console.log(response.data.estado);
+                console.log("estado"+response.data.estado);
                 if(response.data.estado == 1){
                     $scope.data = response.data.historial;
                 //console.log(response.data.historial);
                     //console.log("con datos");
                     $scope.llenarDatos($scope.data);
                 } else {
+                    
                     //console.log("sin datos0");
                 }
 		      
@@ -61,6 +71,8 @@ angular.module('InsuControl').config(['ChartJsProvider', function (ChartJsProvid
             //console.log(arreglo);
             
         }
+        
+        
         
         
 		
